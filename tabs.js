065e51f1c05018948bc3543073dbafc324caa26b
{timon} <{k.poloczek7@gmail.com}>
2022-02-01 17:52:54 +0100

    refactor JS functions for changing tabs

--- tabs.js
+++ tabs.js
@@ -15,24 +15,22 @@ function changeTabFocus(e) {
     
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1)
-    }
+
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
+        }
     
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
-        }
+        tabs[tabFocus].setAttribute("tabindex", 0)
+        tabs[tabFocus].focus()
     }
-
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
-        }
-    }
-
-    tabs[tabFocus].setAttribute("tabindex", 0)
-    tabs[tabFocus].focus()
 }
 
 
@@ -50,15 +48,19 @@ function changeTabPanel(e) {
 
     targetTab.setAttribute("aria-selected", true)
 
-    mainContainer
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach((panel) => panel.setAttribute("hidden", true));
+    hideContent(mainContainer, '[role="tabpanel"]')
+    showContent(mainContainer, [`#${targetPanel}`])
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    hideContent(mainContainer, "picture")
+    showContent(mainContainer, [`#${targetImage}`])
+}
 
-    mainContainer
-        .querySelectorAll('picture')
-        .forEach((picture) => picture.setAttribute("hidden", true));
+function hideContent(parent, content) {
+    parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
 
-    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
+function showContent(parent, content) {
+    parent.querySelector(content).removeAttribute('hidden');
 }
